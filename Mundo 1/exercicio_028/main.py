69b25039914a9e1769413b0f862787cded25032c
Ninniet5670 <marconeto3000@gmail.com>
2021-11-14 03:57:01 -0300

    program and its path fixed

--- Mundo 1/exercicio_028/main.py
+++ Mundo 1/exercicio_028/main.py
@@ -1,20 +1,31 @@
 
 from random import choice
-from playsound import playsound
+# import pyglet
+
 n = [0, 1, 2, 3, 4, 5]
 nn = choice(n)
 pc = int(input('Adivinhe o número de 0 a 5: '))
+
 if pc == nn:
     print('Parabéns você acertou de primeira! :)')
-    playsound('c:/Users/Marco Neto/Desktop/Aulas/Python CursoEmVideo/acerto mizeravi.mp3')
+    # music = pyglet.resource.media('acerto mizeravi.mp3')
+    # music.play()
+    # pyglet.app.run()
 else:
     print('Que pena, você errou o número, que tal uma segunda chance?')
-    playsound('c:/Users/Marco Neto/Desktop/Aulas/Python CursoEmVideo/Errou.mp3')
+    # music = pyglet.resource.media('Errou.mp3')
+    # music.play()
     sc = int(input('Digite outro número de 0 a 5: '))
+
     if sc == nn:
         print('Parabéns, você acertou na segunda tentativa! :)')
-        playsound('c:/Users/Marco Neto/Desktop/Aulas/Python CursoEmVideo/acerto mizeravi.mp3')
+        # music = pyglet.resource.media('acerto mizeravi.mp3')
+        # music.play()
+        # pyglet.app.run()
     else:
         print(f'Você errou todas as chances, seu número era o {nn}')
-        playsound('c:/Users/Marco Neto/Desktop/Aulas/Python CursoEmVideo/ERROU FEIO, ERROU RUDE.mp3')
-        
+        # music = pyglet.resource.media('ERROU FEIO, ERROU RUDE.mp3')
+        # music.play()
+        # pyglet.app.run()
+
+# pyglet comentado devido à erro de tempo de execução
